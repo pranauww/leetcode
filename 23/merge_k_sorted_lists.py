81a1cbaeb4e697ba9b751bc28deaef87d4e93f3c
Pranav Bhartiya <bhartiya.pranav@gmail.com>
2025-12-03 02:31:12 -0500

    fixed bug in problem 23

--- 23/merge_k_sorted_lists.py
+++ 23/merge_k_sorted_lists.py
@@ -21,13 +21,14 @@ class Solution:
         dummy = ListNode()
         tail = dummy
 
-        if l1.val < l2.val:
-            tail.next = l1
-            l1 = l1.next
-        else:
-            tail.next = l2
-            l2 = l2.next
-        tail = tail.next
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
 
         if l1:
             tail.next = l1
